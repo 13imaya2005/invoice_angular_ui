0b3e0d719abb5292d905ade8d4f996eff8f857b6
13imaya2005 <13imaya2005@gmail.com>
2026-03-29 19:05:27 +0530

    angular integration working with api

---
+++ src/app/models/itemmaster.ts
@@ -0,0 +1,15 @@
+export interface Itemmaster
+{
+    id:number;
+    catCode:string;
+    itemBarCode:string;
+    itemCode:string;
+    ItemNsme:string;
+    description?:number;
+    uom:string;
+    rat?:number;
+    minimumStock:number;
+    maximumStock:number;
+    isActive:number;
+}
+
