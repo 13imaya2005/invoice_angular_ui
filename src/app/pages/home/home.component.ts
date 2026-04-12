7aeabaa0802d7ad358fd2d42e4fe0df3e9f5c86b
13imaya2005 <13imaya2005@gmail.com>
2026-04-12 18:50:12 +0530

    jwt related code added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,42 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { AuthService } from '../../services/auth.serfinishedvice';
+import { LoginRequest } from '../../models/loginrequest';
+import { FormsModule } from '@angular/forms';
  
 @Component({
   selector:'app-home',
   standalone:true,
-  imports:[CommonModule],
+  imports:[CommonModule,FormsModule],
   templateUrl:'./home.component.html',
   styleUrl:'./home.component.css'
 })
-export class HomeComponent{}
+export class HomeComponent{
+ isLoginSuccess:boolean |null=null;
+ userName='admin';
+ password='password';
+ message='';
+
+  constructor(private service: AuthService) {}
+  login(){
+    const param:LoginRequest = {
+      userName:this.userName,
+      password:this.password,
+    }
+    this.service.login(param).subscribe({
+      next: (res)=>{
+        this.service.setSession(res.token,
+          res.expiration);
+          this.isLoginSuccess=true;
+          this.message='login successfully';
+        },
+        error:(err) => {
+        this.isLoginSuccess = false;
+        this.message = 'Login Failed';
+        console.error(err);
+      },
+    });
+  }
+ }
+ 
  
